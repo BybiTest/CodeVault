ed37941948b2e96a9d2b1fa01261a6f96e16e0ee
BybiTest <169149340+BybiTest@users.noreply.github.com>
2026-09-26 02:02:19 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,8 +17,13 @@ plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
+    // مخزن اختصاصی تپسل
+    maven { url = uri("https://maven.tapsell.ir") }
+
     google()
     mavenCentral()
+
+    // برای Poolakey (کافه‌بازار)
     maven { url = uri("https://jitpack.io") }
   }
 }
